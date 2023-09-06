0dde5c68c987e1810bdd957d85711232cf6aed9c
oddtO <121516833+oddtO@users.noreply.github.com>
2023-09-06 17:58:17 +0300

    add create todo popup

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
+import "../css/body_editor.css";
+
 import "../css/reset.css";
 import "../css/body.css";
 import "../css/body-header.css";
-import '../css/body-aside.css';
+import "../css/body-aside.css";
