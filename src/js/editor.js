58803644e7bc54b151f39f8f97b71123124feb65
oddtO <121516833+oddtO@users.noreply.github.com>
2023-09-18 05:22:38 +0300

    change, save and cancel in a card editor

--- src/js/editor.js
+++ src/js/editor.js
@@ -26,6 +26,44 @@ export function initEditorListeners(editorElem) {
     hideEditor();
   });
 
+  let editButtonsAll = editorElem.querySelectorAll("label ul");
+  for (const editButtons of editButtonsAll) {
+    editButtons.addEventListener(
+      "click",
+      new (class {
+        handleEvent(event) {
+          let action = event.target.dataset.action;
+          if (!action) return;
+          event.preventDefault();
+          this[action](event);
+        }
+        editField(event) {
+          let target = event.target;
+          let inputLabel = target.closest("label");
+          inputLabel.classList.add("on-edit");
+
+          let input = inputLabel.querySelector("[name]");
+          input.disabled = false;
+          this.input = input;
+          this.inputLabel = inputLabel;
+        }
+        cancelEditField() {
+          this.input[getValueFieldName(this.input)] =
+            Card.cardList[editorElem.dataset.editingIndex][this.input.name];
+          this.disableField();
+        }
+        saveEditField() {
+			Card.cardList[editorElem.dataset.editingIndex][this.input.name] = this.input[getValueFieldName(this.input)];
+					this.disableField();
+
+				}
+        disableField() {
+          this.inputLabel.classList.remove("on-edit");
+          this.input.disabled = true;
+        }
+      })(),
+    );
+  }
   function hideEditor() {
     document.body.className = "";
     editorElem.classList.remove("new");
@@ -35,19 +73,18 @@ export function initEditorListeners(editorElem) {
 export function displayCardInEditor(editorElem, cardElem) {
   let inputs = editorElem.querySelectorAll("[name]");
   let card = Card.cardList[cardElem.dataset.index];
-
+  editorElem.dataset.editingIndex = cardElem.dataset.index;
   for (const input of inputs) {
-    let newValue = card[input.name];
-    input[getValueFieldName(input)] = newValue;
-		if(input.type == 'checkbox') continue;
+    let value = card[input.name];
+    input[getValueFieldName(input)] = value;
+    if (input.type == "checkbox") continue;
     input.disabled = true;
-
   }
+}
 
-  function getValueFieldName(input) {
-    if (input.matches('[type="date"]')) return "valueAsDate";
+function getValueFieldName(input) {
+  if (input.matches('[type="date"]')) return "valueAsDate";
 
-    if (input.matches('[type="checkbox"]')) return "checked";
-    return "value";
-  }
+  if (input.matches('[type="checkbox"]')) return "checked";
+  return "value";
 }
