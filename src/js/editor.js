6012aade4ee5f452474baba40d6f689387c5ed17
oddtO <121516833+oddtO@users.noreply.github.com>
2023-09-19 00:07:06 +0300

    move FieldEditor class to its own file

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,4 +1,5 @@
 import * as Card from "./card.js";
+import { askReRender, getValueFieldName, FieldEditor } from "./field-editor.js";
 
 let editedCardLiElem = Symbol("edited-card");
 
@@ -32,46 +33,14 @@ export function initEditorListeners(editorElem) {
   isCompletedCheckbox.addEventListener("change", (event) => {
     Card.cardList[editorElem.dataset.editingIndex][isCompletedCheckbox.name] =
       isCompletedCheckbox.checked;
-    askReRender(editorElem);
+    askReRender(editorElem, editedCardLiElem);
   });
 
   let editButtonsAll = editorElem.querySelectorAll("label ul");
   for (const editButtons of editButtonsAll) {
     editButtons.addEventListener(
       "click",
-      new (class {
-        handleEvent(event) {
-          let action = event.target.dataset.action;
-          if (!action) return;
-          event.preventDefault();
-          this[action](event);
-        }
-        editField(event) {
-          let target = event.target;
-          let inputLabel = target.closest("label");
-          inputLabel.classList.add("on-edit");
-
-          let input = inputLabel.querySelector("[name]");
-          input.disabled = false;
-          this.input = input;
-          this.inputLabel = inputLabel;
-        }
-        cancelEditField() {
-          this.input[getValueFieldName(this.input)] =
-            Card.cardList[editorElem.dataset.editingIndex][this.input.name];
-          this.disableField();
-        }
-        saveEditField() {
-          Card.cardList[editorElem.dataset.editingIndex][this.input.name] =
-            this.input[getValueFieldName(this.input)];
-          this.disableField();
-          askReRender(editorElem);
-        }
-        disableField() {
-          this.inputLabel.classList.remove("on-edit");
-          this.input.disabled = true;
-        }
-      })(),
+      new FieldEditor(editorElem, editedCardLiElem),
     );
   }
   function hideEditor() {
@@ -93,17 +62,3 @@ export function displayCardInEditor(editorElem, cardLiElem) {
   }
 }
 
-function getValueFieldName(input) {
-  if (input.matches('[type="date"]')) return "valueAsDate";
-
-  if (input.matches('[type="checkbox"]')) return "checked";
-  return "value";
-}
-
-function askReRender(editorElem) {
-  editorElem.dispatchEvent(
-    new CustomEvent("card-change", {
-      detail: { cardLiElem: editorElem[editedCardLiElem] },
-    }),
-  );
-}
---
+++ src/js/field-editor.js
@@ -0,0 +1,54 @@
+import * as Card from "./card.js";
+
+export class FieldEditor {
+  constructor(editorElem, editedCardLiElem) {
+    this.editorElem = editorElem;
+    this.editedCardLiElem = editedCardLiElem;
+  }
+  handleEvent(event) {
+    let action = event.target.dataset.action;
+    if (!action) return;
+    event.preventDefault();
+    this[action](event);
+  }
+  editField(event) {
+    let target = event.target;
+    let inputLabel = target.closest("label");
+    inputLabel.classList.add("on-edit");
+
+    let input = inputLabel.querySelector("[name]");
+    input.disabled = false;
+    this.input = input;
+    this.inputLabel = inputLabel;
+  }
+  cancelEditField() {
+    this.input[getValueFieldName(this.input)] =
+      Card.cardList[this.editorElem.dataset.editingIndex][this.input.name];
+    this.disableField();
+  }
+  saveEditField() {
+    Card.cardList[this.editorElem.dataset.editingIndex][this.input.name] =
+      this.input[getValueFieldName(this.input)];
+    this.disableField();
+    askReRender(this.editorElem, this.editedCardLiElem);
+  }
+  disableField() {
+    this.inputLabel.classList.remove("on-edit");
+    this.input.disabled = true;
+  }
+}
+
+export function askReRender(editorElem, editedCardLiElem) {
+  editorElem.dispatchEvent(
+    new CustomEvent("card-change", {
+      detail: { cardLiElem: editorElem[editedCardLiElem] },
+    }),
+  );
+}
+
+export function getValueFieldName(input) {
+  if (input.matches('[type="date"]')) return "valueAsDate";
+
+  if (input.matches('[type="checkbox"]')) return "checked";
+  return "value";
+}
